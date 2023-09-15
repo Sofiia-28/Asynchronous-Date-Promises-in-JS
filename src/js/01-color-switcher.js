c26596621ad2b11f0b4746528e4903cb15642b79
Sonya1488 <127775334+Sofiia-28@users.noreply.github.com>
2023-09-15 16:18:08 +0300

    color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let timerId = null;
 
+startBtn.addEventListener("click", changeColor);
+function changeColor(event) {
+    timerId = setInterval(() => {
+        function getRandomHexColor() {
+            return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+          };
+          document.body.style.backgroundColor = getRandomHexColor();
+          startBtn.setAttribute('disabled', '');
+      }, 1000);
+}
+
+stopBtn.addEventListener("click", stopChangeColor);
+function stopChangeColor(event) {
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled', '');
+}
+
+ 
