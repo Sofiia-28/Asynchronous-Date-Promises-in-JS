33cd30b11e667c2f5d80014008b8cdfa62775853
Sonya1488 <127775334+Sofiia-28@users.noreply.github.com>
2023-09-17 15:18:37 +0300

    02-timer done

---
+++ src/js/02-timer.js
@@ -0,0 +1,73 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+// Refs
+const inputEl = document.getElementById('datetime-picker');
+const daysNumber = document.querySelector('span[data-days]');
+const hoursNumber = document.querySelector('span[data-hours]');
+const minutesNumber = document.querySelector('span[data-minutes]');
+const secondsNumber = document.querySelector('span[data-seconds]');
+const startBtn = document.querySelector('button[data-start]');
+let timerId = null;
+let currentDate = new Date();
+let selectedDate;
+
+const options = {
+  enableTime: true,
+  enableSeconds: true,
+  time_24hr: true,
+  defaultDate: currentDate,
+  minDate: currentDate,
+  minuteIncrement: 1,
+  allowInput: true,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0];
+    console.log(selectedDate);
+    if (selectedDate <= currentDate) {
+      window.alert('Please choose a date in the future');
+    }
+    startBtn.removeAttribute('disabled', '');
+  },
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+flatpickr(inputEl, options);
+
+startBtn.setAttribute('disabled', '');
+startBtn.addEventListener('click', startCountTime);
+function startCountTime(event) {
+  timerId = setInterval(() => {
+    startBtn.setAttribute('disabled', '');
+    currentDate = new Date();
+    const timeDifferenceMs = selectedDate.getTime() - currentDate.getTime();
+    console.log(currentDate);
+    const timeDifference = convertMs(timeDifferenceMs);
+    const { days, hours, minutes, seconds } = timeDifference;
+    daysNumber.textContent = days.toString().padStart(2, '0');
+    hoursNumber.textContent = hours.toString().padStart(2, '0');
+    minutesNumber.textContent = minutes.toString().padStart(2, '0');
+    secondsNumber.textContent = seconds.toString().padStart(2, '0');
+
+    setTimeout(() => {
+      clearInterval(timerId);
+    }, timeDifferenceMs);
+  }, 1000);
+}
